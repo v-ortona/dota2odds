5cf85783064692c8a0f5ce1b5f78d893876fa1d1
v-ortona <vortona@gmail.com>
2015-09-05 12:29:48 -0700

    50% - 50%

--- calculateodds.php
+++ calculateodds.php
@@ -9,6 +9,7 @@
 
 
     function getTeamElo($team, $conn) {
+        //CONVERT ALL OF DOTA2LOUNGE NAMES TO GOSUGAMERS NAMES
         switch($team) {
             case 'Vega': $team = 'Vega Squadron'; break;
             case 'Na\'Vi': $team = 'Natus Vincere Dota 2'; break;
@@ -17,12 +18,11 @@
             case 'Empire': $team = 'Team Empire Dota2'; break;
             case 'NiP': $team = 'Ninjas in Pyjamas Dota2'; break;
         }
-        $result = $conn->query("SELECT `elo` FROM `gosugamers` WHERE `team` = ' $team'");
+        $result = $conn->query("SELECT `elo` FROM `gosugamers` WHERE `team` = ' $team'"); //THERE IS A SPACE AFTER $TEAM BECAUSE ITS STORED LIKE THAT IN THE DATABASE
         if ($result->num_rows > 0) {
             $elo = mysqli_fetch_assoc($result)['elo'];
         }
         else {
-            echo "No elo found";
             $elo = 0;
         }
         return $elo;
@@ -32,10 +32,11 @@
     function calculateOdds($team1, $team2, $conn) { //Calculate the odds the team 1 beats team 2
         $team1elo = getTeamElo($team1, $conn);
         $team2elo = getTeamElo($team2, $conn);
-        $odds = 1 / (1+pow(10,($team2elo-$team1elo)/400));
+        $odds = 1 / (1+pow(10,($team2elo-$team1elo)/400)); //Elo calculation
+        if($team1elo = 0 || $team2elo = 0) {
+           $odds = 0;
+        }
         return $odds;
     }
 
-    echo calculateOdds('Na\'Vi', 'NiP', $conn);
-    mysqli_close($conn);
 ?>
--- dota2loungescraper.php
+++ dota2loungescraper.php
@@ -1,6 +1,6 @@
 <?php
   require_once('calculateOdds.php');
-  
+
   function getPage($url) {
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
@@ -41,11 +41,53 @@
       echo $team1 . ' ('.$odds1.'%) ' . $team2 . ' ('.$odds2.'%)<br>';
 
 
-      echo '\tACTUAL ODDS: ';
-
+      echo 'ACTUAL ODDS: ';
+      $actualOdds = calculateOdds($team1,$team2,$conn);
+      if($actualOdds < 0.01) {
+        $actualOdds = 'N/A';
+        echo 'Elo of one of these teams cannot be found<br>';
+      }
+      else {
+      $actualOdds = round($actualOdds * 100);
+      echo $team1. ' ('.$actualOdds.'%) ' . $team2 . ' ('.(100-$actualOdds).'%)<br>';
+      }
 
       $i++;
       $j=$j+2;
   }
 
+
+  echo 'FINISHED GAMES:<br>';
+  $match = $loungeXPath->query('//div[@class="match notavailable"]');
+
+  $i=0;
+  $j=0;
+  while($match->item($i)->nodeValue) {
+      $teams = $loungeXPath->query('//div[@class="match notavailable"]//div[@class="teamtext"]');
+
+      $team1 = substr($teams->item($j)->nodeValue, 0, -3);
+        $odds1 = substr($teams->item($j)->nodeValue, -3, -1);
+      $team2 = substr($teams->item($j+1)->nodeValue, 0, -3);
+        $odds2 = substr($teams->item($j+1)->nodeValue, -3, -1);
+
+      echo $team1 . ' ('.$odds1.'%) ' . $team2 . ' ('.$odds2.'%)<br>';
+
+
+      echo 'ACTUAL ODDS: ';
+      $actualOdds = calculateOdds($team1,$team2,$conn);
+      if($actualOdds < 0.01 || $actualOdds > 0.99) {
+        $actualOdds = 'N/A';
+        echo 'Elo of one of these teams cannot be found<br>';
+      }
+      else {
+      $actualOdds = round($actualOdds * 100);
+      echo $team1. ' ('.$actualOdds.'%) ' . $team2 . ' ('.(100-$actualOdds).'%)<br>';
+      }
+
+      $i++;
+      $j=$j+2;
+  }
+
+
+  mysqli_close($conn);
 ?>
